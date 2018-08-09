ae9574934141cfd3762befbb731cd9563e837eab
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-09 22:16:44 +0900

    Add files via upload

---
+++ IntelLibva_Start.cpp
@@ -0,0 +1,16 @@
+/*
+ * IntelLibva_Start.cpp
+ *
+ *  Created on: Aug 9, 2018
+ *      Author: Tung Dang
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Let's to start" << endl;
+}
+
+
