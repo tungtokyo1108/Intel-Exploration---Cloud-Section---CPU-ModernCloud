9132d4a7e7200ab1e8f104cfd3968816f18a25e3
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-12 16:07:46 +0900

    Add files via upload

---
+++ include/qemu/queue.h
@@ -0,0 +1,14 @@
+/*
+ * queue.h
+ *
+ *  Created on: Aug 12, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QEMU_QUEUE_H_
+#define QEMU_QUEUE_H_
+
+#include "qemu/atomic.h"
+
+
+#endif /* QEMU_QUEUE_H_ */
