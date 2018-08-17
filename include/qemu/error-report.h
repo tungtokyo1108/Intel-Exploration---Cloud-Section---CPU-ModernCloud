827e058797567d20084a4ad0217168a613de572c
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-17 15:01:43 +0900

    Add files via upload

---
+++ include/qemu/error-report.h
@@ -0,0 +1,13 @@
+/*
+ * error-report.h
+ *
+ *  Created on: Aug 17, 2018
+ *      Author: tungdang
+ */
+
+#ifndef QEMU_ERROR_REPORT_H_
+#define QEMU_ERROR_REPORT_H_
+
+
+
+#endif /* QEMU_ERROR_REPORT_H_ */
