be25221933de777e852024069f9087166fd0b07e
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-17 16:03:44 +0900

    Add files via upload

---
+++ include/qemu/config-file.h
@@ -0,0 +1,27 @@
+/*
+ * config-file.h
+ *
+ *  Created on: Aug 17, 2018
+ *      Author: tungdang
+ */
+
+#ifndef QEMU_CONFIG_FILE_H_
+#define QEMU_CONFIG_FILE_H_
+
+#include "qemu/option.h"
+
+QemuOptsList *qemu_find_opts(const char *group);
+QemuOpts *qemu_find_opts_singleton(const char *group);
+
+void qemu_add_opts(QemuOptsList *list);
+void qemu_add_drive_opts(QemuOptsList *list);
+int qemu_set_option(const char *str);
+int qemu_global_option(const char *str);
+
+void qemu_config_write(FILE *fp);
+int qemu_config_parse(FILE *fp, QemuOptsList **lists, const char *fname);
+int qemu_read_config_file(const char *filename);
+
+// void qemu_config_parse_qdict(QDict *options, QemuOptsList **lists, Error **errp);
+
+#endif /* QEMU_CONFIG_FILE_H_ */
