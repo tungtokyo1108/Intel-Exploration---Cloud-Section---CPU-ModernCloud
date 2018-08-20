b6cc390a23e1949fd8a24acc5f68b188012cba96
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-20 13:51:50 +0900

    Update cpuid.h

--- include/qemu/cpuid.h
+++ include/qemu/cpuid.h
@@ -15,6 +15,34 @@
 #endif
 
 #ifndef bit_SSE2
+#define bit_SSE2 (1 << 26)
+#endif
+
+#ifndef bit_SSE4_1
+#define bit_SSE4_1      (1 << 19)
+#endif
+#ifndef bit_MOVBE
+#define bit_MOVBE       (1 << 22)
+#endif
+#ifndef bit_OSXSAVE
+#define bit_OSXSAVE     (1 << 27)
+#endif
+#ifndef bit_AVX
+#define bit_AVX         (1 << 28)
+#endif
+
+#ifndef bit_BMI
+#define bit_BMI         (1 << 3)
+#endif
+#ifndef bit_AVX2
+#define bit_AVX2        (1 << 5)
+#endif
+#ifndef bit_BMI2
+#define bit_BMI2        (1 << 8)
+#endif
+
+#ifndef bit_LZCNT
+#define bit_LZCNT       (1 << 5)
 #endif
 
 #endif /* QEMU_CPUID_H_ */
