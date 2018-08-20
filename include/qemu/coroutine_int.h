428fe5c87da1ac95be491f82dacc11cbf0f39b71
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-20 11:34:32 +0900

    Update coroutine_int.h

--- include/qemu/coroutine_int.h
+++ include/qemu/coroutine_int.h
@@ -8,6 +8,31 @@
 #ifndef QEMU_COROUTINE_INT_H_
 #define QEMU_COROUTINE_INT_H_
 
+#include "qemu/queue.h"
+#include "qemu/coroutine.h"
 
+#define COROUTINE_STACK_SIZE (1 << 20)
+typedef enum {
+	COROUTINE_YIELD = 1,
+	COROUTINE_TERMINATE = 2,
+	COROUTINE_ENTER = 3,
+} CoroutineAction;
+
+struct Coroutine {
+	CoroutineEntry *entry;
+	void *entry_arg;
+	Coroutine *caller;
+	QSLIST_ENTRY(Coroutine) pool_next;
+	size_t locks_held;
+
+	const char *scheduled;
+	QSIMPLEQ_ENTRY(Coroutine) co_queue_next;
+	QSIMPLEQ_HEAD(, Coroutine) co_queue_wakeup;
+	QLIST_ENTRY(Coroutine) co_scheduled_next;
+};
+
+Coroutine *qemu_coroutine_new(void);
+void qemu_coroutine_delete(Coroutine *co);
+CoroutineAction qemu_coroutine_switch(Coroutine *from, Coroutine *to, CoroutineAction action);
 
 #endif /* QEMU_COROUTINE_INT_H_ */
