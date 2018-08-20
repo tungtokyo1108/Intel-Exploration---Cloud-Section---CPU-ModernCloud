379411830077d174cb010e28c9834c5026b231a7
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-20 23:04:34 +0900

    Add files via upload

---
+++ include/qemu/hbitmap.h
@@ -0,0 +1,19 @@
+/*
+ * hbitmap.h
+ *
+ *  Created on: Aug 20, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QEMU_HBITMAP_H_
+#define QEMU_HBITMAP_H_
+
+#include "bitops.h"
+#include "host-utils.h"
+
+typedef struct HBitmap HBitmap;
+typedef struct HBitmapIter HBitmapIter;
+
+
+
+#endif /* QEMU_HBITMAP_H_ */
