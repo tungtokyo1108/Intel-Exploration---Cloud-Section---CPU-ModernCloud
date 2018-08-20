7d52df60d07593d3f8b33c0492b204c31ce42986
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-21 00:18:03 +0900

    Add files via upload

---
+++ include/qemu/typedefs.h
@@ -0,0 +1,13 @@
+/*
+ * typedefs.h
+ *
+ *  Created on: Aug 21, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QEMU_TYPEDEFS_H_
+#define QEMU_TYPEDEFS_H_
+
+
+
+#endif /* QEMU_TYPEDEFS_H_ */
