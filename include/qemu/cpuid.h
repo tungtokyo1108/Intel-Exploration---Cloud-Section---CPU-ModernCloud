f8da92329e6bb009b0fff0eeb834bfac5a869437
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-20 11:40:52 +0900

    Add files via upload

---
+++ include/qemu/cpuid.h
@@ -0,0 +1,20 @@
+/*
+ * cpuid.h
+ *
+ *  Created on: Aug 20, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QEMU_CPUID_H_
+#define QEMU_CPUID_H_
+
+#include <cpuid.h>
+
+#ifndef bit_CMOV
+#define bit_CMOV (1 << 15)
+#endif
+
+#ifndef bit_SSE2
+#endif
+
+#endif /* QEMU_CPUID_H_ */
