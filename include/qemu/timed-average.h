7842e36c8736cb283979cef3da9207f544d455b6
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-21 11:34:21 +0900

    Add files via upload

---
+++ include/qemu/timed-average.h
@@ -0,0 +1,13 @@
+/*
+ * timed-average.h
+ *
+ *  Created on: Aug 21, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QEMU_TIMED_AVERAGE_H_
+#define QEMU_TIMED_AVERAGE_H_
+
+
+
+#endif /* QEMU_TIMED_AVERAGE_H_ */
