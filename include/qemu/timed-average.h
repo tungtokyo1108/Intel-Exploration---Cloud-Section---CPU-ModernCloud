11f0ab55cbb47d2308323f9e746ddc1c09d83869
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-21 13:45:08 +0900

    Update timed-average.h

--- include/qemu/timed-average.h
+++ include/qemu/timed-average.h
@@ -8,6 +8,32 @@
 #ifndef QEMU_TIMED_AVERAGE_H_
 #define QEMU_TIMED_AVERAGE_H_
 
+#include "qemu/timer.h"
+#include <stdint.h>
 
+typedef struct TimedAverageWindow TimedAverageWindow;
+typedef struct TimedAverage TimedAverage;
+
+struct TimedAverageWindow {
+	uint64_t min;
+	uint64_t max;
+	uint64_t sum;
+	uint64_t count;
+	int64_t expiration;
+};
+
+struct TimedAverage {
+	uint64_t period;
+	TimedAverageWindow windows[2];
+	unsigned current;
+	QEMUClockType clock_type;
+};
+
+void timed_average_init(TimedAverage *ta, QEMUClockType clock_type, uint64_t period);
+void timed_average_account(TimedAverage *ta, uint64_t value);
+uint64_t timed_average_min(TimedAverage *ta);
+uint64_t timed_average_avg(TimedAverage *ta);
+uint64_t timed_average_max(TimedAverage *ta);
+uint64_t timed_average_sum(TimedAverage *ta, uint64_t *elapsed);
 
 #endif /* QEMU_TIMED_AVERAGE_H_ */
