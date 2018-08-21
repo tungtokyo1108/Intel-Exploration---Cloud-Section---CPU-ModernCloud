b1d3a542263e868c57a464af7be4e6c46d5c46ad
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-21 14:25:31 +0900

    Update id.h

--- include/qemu/id.h
+++ include/qemu/id.h
@@ -8,6 +8,13 @@
 #ifndef QEMU_ID_H_
 #define QEMU_ID_H_
 
+typedef enum IdSubSystem {
+	ID_QDEV,
+	ID_BLOCK,
+	ID_MAX
+} IdSubSystems;
 
+char *id_generate(IdSubSystems id);
+bool id_wellformed(const char *id);
 
 #endif /* QEMU_ID_H_ */
