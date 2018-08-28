406922d57e87fc7226180fab200d7865d795c607
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-28 11:04:01 +0900

    Add files via upload

---
+++ include/qemu/job.h
@@ -0,0 +1,13 @@
+/*
+ * job.h
+ *
+ *  Created on: Aug 28, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QEMU_JOB_H_
+#define QEMU_JOB_H_
+
+
+
+#endif /* QEMU_JOB_H_ */
---
+++ include/qemu/ratelimit.h
@@ -0,0 +1,13 @@
+/*
+ * ratelimit.h
+ *
+ *  Created on: Aug 28, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QEMU_RATELIMIT_H_
+#define QEMU_RATELIMIT_H_
+
+
+
+#endif /* QEMU_RATELIMIT_H_ */
