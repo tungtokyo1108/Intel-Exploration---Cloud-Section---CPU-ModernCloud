fd4bf38c4ddccdb9f29dc92170de2d7ff04b467a
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-30 18:44:52 -0700

    Add files via upload

---
+++ include/qapi/qmp/qjson.h
@@ -0,0 +1,13 @@
+/*
+ * qjson.h
+ *
+ *  Created on: Aug 31, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QAPI_QMP_QJSON_H_
+#define QAPI_QMP_QJSON_H_
+
+
+
+#endif /* QAPI_QMP_QJSON_H_ */
---
+++ include/qapi/qmp/qnull.h
@@ -0,0 +1,13 @@
+/*
+ * qnull.h
+ *
+ *  Created on: Aug 31, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QAPI_QMP_QNULL_H_
+#define QAPI_QMP_QNULL_H_
+
+
+
+#endif /* QAPI_QMP_QNULL_H_ */
