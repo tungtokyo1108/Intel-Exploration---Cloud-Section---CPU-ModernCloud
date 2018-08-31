ca9d285250a053fd3fcb4769203521d08775e032
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-31 14:44:24 +0900

    Update qobject-output-visitor.h

--- include/qapi/qobject-output-visitor.h
+++ include/qapi/qobject-output-visitor.h
@@ -8,6 +8,10 @@
 #ifndef QAPI_QOBJECT_OUTPUT_VISITOR_H_
 #define QAPI_QOBJECT_OUTPUT_VISITOR_H_
 
+#include "qapi/visitor.h"
 
+typedef struct QObjectOutputVisitor QObjectOutputVisitor;
+
+Visitor *qobject_output_visitor_new(QObject **result);
 
 #endif /* QAPI_QOBJECT_OUTPUT_VISITOR_H_ */
