f998106308034ac05e798b030dd9a3f32e6621c9
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-31 11:05:22 +0900

    Update qnull.h

--- include/qapi/qmp/qnull.h
+++ include/qapi/qmp/qnull.h
@@ -8,6 +8,18 @@
 #ifndef QAPI_QMP_QNULL_H_
 #define QAPI_QMP_QNULL_H_
 
+#include "qapi/qmp/qobject.h"
 
+typedef struct QNull QNull;
+
+typedef struct QNull {
+	QObjectBase_ base;
+} QNull;
+
+static inline QNull *qnull(void) {
+	return qobject_ref(&qnull_);
+}
+
+bool qnull_is_equal(const QObject *x, const QObject *y);
 
 #endif /* QAPI_QMP_QNULL_H_ */
