ddc1480225b97f24e45230d18319d301db355b35
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-03 21:27:41 +0900

    Update hwaddr.h

--- include/exec/hwaddr.h
+++ include/exec/hwaddr.h
@@ -8,6 +8,18 @@
 #ifndef EXEC_HWADDR_H_
 #define EXEC_HWADDR_H_
 
+#include <stdint.h>
 
+#define HWADDR_BITS 64
+
+typedef uint64_t hwaddr;
+#define HWADDR_MAX UINT64_MAX
+#define TARGET_FMT_plx "%016" PRIx64
+#define HWADDR_PRId PRId64
+#define HWADDR_PRIi PRIi64
+#define HWADDR_PRIo PRIo64
+#define HWADDR_PRIu PRIu64
+#define HWADDR_PRIx PRIx64
+#define HWADDR_PRIX PRIX64
 
 #endif /* EXEC_HWADDR_H_ */
