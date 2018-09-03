57f57f8712210bcb1afe58d17b89b7e02a2a520c
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-03 21:52:02 +0900

    Update fprintf-fn.h

--- include/qemu/fprintf-fn.h
+++ include/qemu/fprintf-fn.h
@@ -9,7 +9,7 @@
 #define QEMU_FPRINTF_FN_H_
 #include <stdio.h>
 
-typedef int  (*fprintf_function)(FILE *f, const char *fmt, ...)
-		GCC_FMT_ATTR(2,3);
+#define GCC_FMT_ATTR(n,m) __attribute__((format(gmu_printf,n,m)))
+typedef int  (*fprintf_function)(FILE *f, const char *fmt, ...) GCC_FMT_ATTR(2,3);
 
 #endif /* QEMU_FPRINTF_FN_H_ */
