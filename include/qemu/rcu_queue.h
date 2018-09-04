10e8ec3eebf4655e7a20300ac6a0c1ec44376d36
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-03 17:22:46 -0700

    Add files via upload

---
+++ include/qemu/rcu_queue.h
@@ -0,0 +1,24 @@
+/*
+ * rcu_queue.h
+ *
+ *  Created on: Sep 4, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QEMU_RCU_QUEUE_H_
+#define QEMU_RCU_QUEUE_H_
+
+#include "qemu/queue.h"
+#include "qemu/atomic.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* QEMU_RCU_QUEUE_H_ */
