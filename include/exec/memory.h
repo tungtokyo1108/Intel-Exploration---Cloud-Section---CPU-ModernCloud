72f2b21c1c297f887e59c8227b7b5108bd01dc63
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-06 15:20:56 +0900

    Update memory.h

--- include/exec/memory.h
+++ include/exec/memory.h
@@ -248,4 +248,195 @@ typedef struct MemoryListener {
 	QTAILQ_ENTRY(MemoryListener) link_as;
 } MemoryListener;
 
+typedef struct AddressSpace AddressSpace;
+typedef struct AddressSpace {
+	struct rcu_head rcu;
+	char *name;
+	MemoryRegion *root;
+	FlatView *current_map;
+	int ioeventfd_nb;
+	MemoryRegionIoeventfd *ioeventfds;
+	QTAILQ_HEAD(memory_listener_as, MemoryListener) listeners;
+	QTAILQ_ENTRY(AddressSpace) address_spaces_link;
+} AddressSpace;
+
+typedef struct AddressSpaceDispatch AddressSpaceDispatch;
+typedef struct FlatRange FlatRange;
+typedef struct FlatView FlatView;
+typedef struct FlatView {
+	struct rcu_head rcu;
+	unsigned ref;
+	FlatRange *ranges;
+	unsigned nr;
+	unsigned nr_allocated;
+	struct AddressSpaceDispatch *dispatch;
+	MemoryRegion *root;
+} FlatView;
+
+static inline FlatView *address_space_to_flatview(AddressSpace *as) {
+	return atomic_rcu_read(&as->current_map);
+}
+
+typedef struct MemoryRegionSection MemoryRegionSection;
+typedef struct MemoryRegionSection {
+	MemoryRegion *mr;
+	FlatView *fv;
+	hwaddr offset_within_region;
+	Int128 size;
+	hwaddr offset_within_address_space;
+	bool readonly;
+} MemoryRegionSection;
+
+// the region typically acts as a container for other memory regions
+
+void memory_region_init(MemoryRegion *mr, Object *owner, const char *name, uint64_t size);
+
+/**
+ * Whenever memory regions are accessed outside the BQL, they need to be preserved against hot-unplug,
+ * All MemoryRegions must have an owner if they can disappear, even if the device they belong to operates under the BQL.
+ */
+
+void memory_region_ref(MemoryRegion *mr);
+void memory_region_unref(MemoryRegion *mr);
+
+// Access into the region will cause the callbacks in @op to be called. If @size is nonzero, subregions will be clipped to @size
+
+void memory_region_init_io(MemoryRegion *mr, Object *owner, const MemoryRegionOps *ops, void *opaque, const char *name, uint64_t size);
+
+// Initialize RAM memory region. Accesses into the region will modify memory directly.
+
+void memory_region_init_ram_nomigrate(MemoryRegion *mr, Oject *owner, const char *name, uint64_t size, Error **errp);
+void memory_region_init_ram_shared_nomigrate(MemoryRegion *mr, Oject *owner, const char *name, uint64_t size, bool share, Error **errp);
+
+// Initialize memory region with resizeable RAM. Only an initial portion of this RAM is actually used.
+// The used size can change across reboots
+
+void memory_region_init_resizeable_ram(MemoryRegion *mr, Object *owner, const char *name, uint64_t size, uint64_t max_size,
+		                               void (*resized)(const char *, uint64_t length, void *host), Error **errp);
+
+// Initialize memory region with a mmap-end backend
+void memory_region_init_ram_from_file(MemoryRegion *mr, Object *owner, const char *name, uint64_t size, uint64_t align,
+		                              bool share, const char *path, Error **errp);
+void memory_region_init_ram_from_fd(MemoryRegion *mr, Object *owner, const char *name, uint64_t size, uint64_t align,
+		                              bool share, int fd, Error **errp);
+
+// Initialize RAM memory region from a user-provided pointer.
+void memory_region_init_ram_ptr(MemoryRegion *mr, Oject *owner, const char *name, uint64_t size, void *ptr);
+
+/**
+ * A RAM device represents a mapping to a physical device.
+ * The memory region may be mapped into the VM address space and access to the region will modify directly
+ * The memory region should not be included in a memory dump, device may not be enabled/mapped at the time of the dump
+ */
+void memory_region_init_ram_device_ptr(MemoryRegion *mr, Oject *owner, const char *name, uint64_t size, void *ptr);
+
+// Initialize a memory region that aliase all or a part of another memory region.
+void memory_region_init_alias(MemoryRegion *mr, Object *owner, const char *name, MemoryRegion *orig, hwaddr offset, uint64_t size);
+
+// Initialize a ROM memory region.
+void memory_region_init_rom_nomigrate(MemoryRegion *mr, Oject *owner, const char *name, uint64_t size, Error **errp);
+
+// Initialize a ROM memory region, write are handled via callbacks
+void memory_region_init_rom_device_nomigrate(MemoryRegion *mr, Oject *owner,const MemoryRegionOps *ops, void *opaque,
+		                                     const char *name, uint64_t size, Error **errp);
+
+/**
+ * Initialize a memory region of a custom type that translate addresses
+ * An IOMMU region translate addresses and forwards accesses to a target memory region.
+ * IOMMU implementation must define a subclass of TYPE_IOMMU_MEMORY_REGION.
+ * @_iommu_mr should be a pointer to enough memory for an instance of that subclass
+ * @instance_size is  the size of the subclass
+ * will be called to handle accesses to memory region
+ */
+
+void memory_region_init_iommu(void *_iommu_mr, size_t instance_size, const char *mrtypename, Object *owner, const char *name, uint64_t size);
+
+/**
+ * Initialize RAM memory region.
+ * This function allocates RAM for a board model or device,
+ * arranges for it to be migrated
+ */
+
+void memory_region_init_ram(MemoryRegion *mr, Object *owner, const char *name, uint64_t size, Error **errp);
+
+/**
+ * Initialize a ROM memory region
+ * This has the same effect as calling memory_region_init_ram()
+ * marking region read-only with memory_region_set_readonly().
+ * arranging for the contents to be migrated
+ */
+
+void memory_region_init_rom(MemoryRegion *mr, Object *owner, const char *name, uint64_t size, Error **errp);
+
+/**
+ * Initialize a ROM memory region. Writes are handled via callbacks
+ * This function initializes a memory region backed by RAM or reads and callbacks for writes
+ * arranges for the RAM backing to be migrated
+ */
+
+void memory_region_init_rom_device(MemoryRegion *mr, Object *owner,const MemoryRegionOps *ops, void *opaque, const char *name, uint64_t size, Error **errp);
+
+// get a memory region's owner
+struct Object *memory_region_owner(MemoryRegion *mr);
+uint64_t memory_region_size(MemoryRegion *mr);
+
+// check whether a memory region is random access
+static inline bool memory_region_is_ram(MemoryRegion *mr) {
+	return mr->ram;
+}
+
+// check whether a memory region is a random device
+bool memory_region_is_ram_device(MemoryRegion *mr);
+
+// check whether a memory region is in ROMD mode
+static inline bool memory_region_is_romd(MemoryRegion *mr) {
+	return mr->rom_device && mr->romd_mode;
+}
+
+// check whether a memory region is an iommu
+static inline IOMMUMemoryRegion *memory_region_get_iommu(MemoryRegion *mr) {
+	if (mr->alias)
+	{
+		return memory_region_get_iommu(mr->alias);
+	}
+	if (mr->is_iommu)
+	{
+		return (IOMMUMemoryRegion *) mr;
+	}
+	return NULL;
+}
+
+static inline IOMMUMemoryRegionClass *memory_region_get_iommu_class_nocheck(IOMMUMemoryRegion *iommu_mr) {
+	return (IOMMUMemoryRegionClass *) (((Object *)iommu_mr));
+}
+
+#define memory_region_is_iommu(mr) (memory_region_get_iommu(mr) != NULL)
+
+// get minimum supported page size for an iommu
+
+uint64_t memory_region_iommu_get_min_page_size(IOMMUMemoryRegion *iommu_mr);
+
+/**
+ * Notify a change in an IOMMU translation entry
+ * Notification type will be decided by entry.perm bits
+ * @UNMAP (cache invalidation) notifier: set entry.perm to IOMMU_NONE
+ * @MAP (newly added entry) notifier: set entry.perm to the permission to the page
+ */
+
+void memory_region_notify_iommu(IOMMUMemoryRegion *iommu_mr, int iommu_idx, IOMMUTLBEntry entry);
+
+// notify a change in an IOMMU translation entry to a single notifier
+
+void memory_region_notify_one(IOMMUNotifier *notifier, IOMMUTLBEntry *entry);
+
+// register a notifier for changes to IOMMU translation entries
+void memory_region_register_iommu_notifier(MemoryRegion *mr, IOMMUNotifier *n);
+
+// replay existing IOMMU translations to a notifier with the minimum page
+void memory_region_iommu_replay(IOMMUMemoryRegion *iommu_mr, IOMMUNotifier *n);
+void memory_region_iommu_replay_all(IOMMUMemoryRegion *iommu_mr);
+
+// unregister a notifier for changes to IOMMU translation entries
+void memory_region_unregister_iommu_notifier(MemoryRegion *mr, IOMMUNotifier *n);
+
 #endif /* EXEC_MEMORY_H_ */
