9c1c1e70548fc780d847537d0463f77c08a1f48c
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-07 13:39:05 +0900

    Update address-spaces.h

--- include/exec/address-spaces.h
+++ include/exec/address-spaces.h
@@ -8,6 +8,16 @@
 #ifndef EXEC_ADDRESS_SPACES_H_
 #define EXEC_ADDRESS_SPACES_H_
 
+#include "exec/memory.h"
 
+#ifndef CONFIG_USER_ONLY
+
+MemoryRegion *get_system_memory(void);
+MemoryRegion *get_system_io(void);
+
+extern AddressSpace address_space_memory;
+extern AddressSpace address_space_io;
+
+#endif
 
 #endif /* EXEC_ADDRESS_SPACES_H_ */
