b27c2e037bbe9472c1448ad60e3ef9b9170293b2
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-16 22:24:18 +0900

    Update log-for-trace.h

--- include/qemu/log-for-trace.h
+++ include/qemu/log-for-trace.h
@@ -8,6 +8,13 @@
 #ifndef QEMU_LOG_FOR_TRACE_H_
 #define QEMU_LOG_FOR_TRACE_H_
 
+extern int qemu_loglevel;
+#define LOG_TRACE (1 << 15)
 
+static inline bool qemu_loglevel_mask(int mask) {
+	return (qemu_loglevel & mask) != 0;
+}
+
+int GCC_FMT_ATTR(1, 2) qemu_log(const char *fmt, ...);
 
 #endif /* QEMU_LOG_FOR_TRACE_H_ */
