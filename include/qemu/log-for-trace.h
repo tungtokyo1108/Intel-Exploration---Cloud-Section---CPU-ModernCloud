1f7277466b31a578437c05c4cfa53277a854addf
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-16 05:45:06 -0700

    Add files via upload

---
+++ include/qemu/log-for-trace.h
@@ -0,0 +1,13 @@
+/*
+ * log-for-trace.h
+ *
+ *  Created on: Sep 16, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QEMU_LOG_FOR_TRACE_H_
+#define QEMU_LOG_FOR_TRACE_H_
+
+
+
+#endif /* QEMU_LOG_FOR_TRACE_H_ */
---
+++ include/qemu/log.h
@@ -0,0 +1,13 @@
+/*
+ * log.h
+ *
+ *  Created on: Sep 16, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef QEMU_LOG_H_
+#define QEMU_LOG_H_
+
+
+
+#endif /* QEMU_LOG_H_ */
