4bfe6bbe336bacf836e8b5af74f0759ecb3b3ca6
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-16 21:20:51 +0900

    Update tb-context.h

--- include/exec/tb-context.h
+++ include/exec/tb-context.h
@@ -8,6 +8,20 @@
 #ifndef EXEC_TB_CONTEXT_H_
 #define EXEC_TB_CONTEXT_H_
 
+#include "qemu/thread.h"
+#include "qemu/qht.h"
 
+#define CODE_GEN_HTABLE_BITS 15
+#define CODE_GEN_HTABLE_SIZE (1 << CODE_GEN_HTABLE_BITS)
+
+typedef struct TranslationBlock TranslationBlock;
+typedef struct TBContext TBContext;
+
+struct TBContext {
+	struct qht htable;
+	unsigned tb_flush_count;
+};
+
+extern TBContext tb_ctx;
 
 #endif /* EXEC_TB_CONTEXT_H_ */
