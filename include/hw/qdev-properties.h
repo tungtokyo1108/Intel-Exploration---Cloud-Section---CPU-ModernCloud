325cf6e87c6003c88c09b9ea443cab3a07f8035e
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-17 14:17:09 +0900

    Add files via upload

---
+++ include/hw/qdev-properties.h
@@ -0,0 +1,14 @@
+/*
+ * qdev-properties.h
+ *
+ *  Created on: Sep 17, 2018
+ *      Student (coder): Tung Dang
+ */
+
+#ifndef HW_QDEV_PROPERTIES_H_
+#define HW_QDEV_PROPERTIES_H_
+
+#include "hw/qdev-core.h"
+
+
+#endif /* HW_QDEV_PROPERTIES_H_ */
