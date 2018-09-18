66cbac646833568e0b49af538f0ec6d8a1fa5b91
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-18 10:50:29 +0900

    Update accel.h

--- include/sysemu/accel.h
+++ include/sysemu/accel.h
@@ -11,6 +11,36 @@
 #include "qom/object.h"
 #include "hw/qdev-properties.h"
 
+typedef struct AccelState {
+	Object parent_obj;
+} AccelState;
 
+typedef struct AccelClass {
+	ObjectClass parent_class;
+
+	const char *opt_name;
+	const char *name;
+	int (*available)(void);
+	int (*int_machine)(MachineState *ms);
+	void (*setup_post)(MachineState *ms, AccelState *accel);
+	bool *allowed;
+	GlobalProperty *global_props;
+} AccelClass;
+
+#define TYPE_ACCEL "accel"
+#define ACCEL_CLASS_SUFFIX "-" TYPE_ACCEL
+#define ACCEL_CLASS_NAME(a) (a ACCEL_CLASS_SUFFIX)
+
+#define ACCEL_CLASS(klass) \
+	OBJECT_CLASS_CHECK(AccelClass, (klass), TYPE_ACCEL)
+#define ACCEL(obj) \
+	OBJECT_CHECK(AccelState, (obj), TYPE_ACCEL)
+#define ACCEL_GET_CLASS(obj) \
+	OBJECT_GET_CLASS(AccelClass, (obj), TYPE_ACCEL)
+
+extern unsigned long tcg_tb_size;
+void configure_accelerator(MachineState *ms);
+void accel_register_compat_props(AccelState *accel);
+void accel_setup_post(MachineState *ms);
 
 #endif /* SYSEMU_ACCEL_H_ */
