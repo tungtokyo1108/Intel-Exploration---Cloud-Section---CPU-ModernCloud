0f5269390a8ab6aaeadbea9b4fea60c5a4de53aa
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-18 10:54:59 +0900

    Update whpx.h

--- include/sysemu/whpx.h
+++ include/sysemu/whpx.h
@@ -10,5 +10,27 @@
 
 #include "qemu-common.h"
 
+// Window Hypervisor Platform accelerator support
+
+int whpx_init_vcpu(CPUState *cpu);
+int whpx_vcpu_exec(CPUState *cpu);
+void whpx_destroy_vcpu(CPUState *cpu);
+void whpx_vcpu_kick(CPUState *cpu);
+
+
+void whpx_cpu_synchronize_state(CPUState *cpu);
+void whpx_cpu_synchronize_post_reset(CPUState *cpu);
+void whpx_cpu_synchronize_post_init(CPUState *cpu);
+void whpx_cpu_synchronize_pre_loadvm(CPUState *cpu);
+
+#ifdef CONFIG_WHPX
+
+int whpx_enabled(void);
+
+#else /* CONFIG_WHPX */
+
+#define whpx_enabled() (0)
+
+#endif
 
 #endif /* SYSEMU_WHPX_H_ */
