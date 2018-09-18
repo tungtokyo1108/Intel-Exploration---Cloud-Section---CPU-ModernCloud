a04945840957dee2439e497d0099ffac17c98524
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-18 13:40:02 +0900

    Update hax.h

--- include/sysemu/hax.h
+++ include/sysemu/hax.h
@@ -13,4 +13,28 @@
 #include "qemu/bitops.h"
 #include "exec/memory.h"
 
+int hax_sync_vcpus(void);
+int hax_init_vcpu(CPUState *cpu);
+int hax_smp_cpu_exec(CPUState *cpu);
+int hax_populate_ram(uint64_t va, uint64_t size);
+
+void hax_cpu_synchronize_state(CPUState *cpu);
+void hax_cpu_synchronize_post_reset(CPUState *cpu);
+void hax_cpu_synchronize_post_init(CPUState *cpu);
+void hax_cpu_synchronize_pre_loadvm(CPUState *cpu);
+
+#ifdef CONFIG_HAX
+
+int hax_enabled(void);
+
+int hax_vcpu_destroy(CPUState *cpu);
+void hax_raise_event(CPUState *cpu);
+void hax_reset_vcpu_state(void *opaque);
+
+#else /* CONFIG_HAX */
+
+#define hax_enabled() (0)
+
+#endif
+
 #endif /* SYSEMU_HAX_H_ */
