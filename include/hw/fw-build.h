41d28cc86990757d2c18d3eec8adc62e1d52bcbc
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-19 20:05:42 +0900

    Update fw-build.h

--- include/hw/fw-build.h
+++ include/hw/fw-build.h
@@ -18,9 +18,10 @@ typedef struct BIOSLinker {
 bool bios_linker_loader_can_write_pointer(void);
 BIOSLinker *bios_linker_loader_init(void);
 void bios_linker_loader_alloc(BIOSLinker *linker, const char *file_name, GArray *file_blob, uint32_t alloc_align, bool alloc_fseg);
-void bios_linker_loader_add_checksum(BIOSLinker *linker, const char *file, unsigned start_offset, unsigned size, unsigned checksum_offset);
-void bios_linker_loader_add_pointer(BIOSLinker *linker, const char *dest_file, uint32_t dst_patched_offset, uint8_t dst_patched_size,
-		                            const char *src_file, uint32_t src_offset);
+void bios_linker_loader_add_checksum(BIOSLinker *linker, const char *file, unsigned start_offset, unsigned size, 
+				     unsigned checksum_offset);
+void bios_linker_loader_add_pointer(BIOSLinker *linker, const char *dest_file, uint32_t dst_patched_offset, 
+				    uint8_t dst_patched_size, const char *src_file, uint32_t src_offset);
 
 
 #endif /* HW_FW_BUILD_H_ */
