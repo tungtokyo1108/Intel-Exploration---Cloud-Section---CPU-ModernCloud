ee3a1ce888eca6b0c9e244c9a1973390c51af367
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-09-19 20:08:04 +0900

    Update fw-build.h

--- include/hw/fw-build.h
+++ include/hw/fw-build.h
@@ -22,6 +22,8 @@ void bios_linker_loader_add_checksum(BIOSLinker *linker, const char *file, unsig
 				     unsigned checksum_offset);
 void bios_linker_loader_add_pointer(BIOSLinker *linker, const char *dest_file, uint32_t dst_patched_offset, 
 				    uint8_t dst_patched_size, const char *src_file, uint32_t src_offset);
-
+void bios_linker_loader_write_pointer(BIOSLinker *linker, const char *dest_file, uint32_t dst_patched_offset,
+		                              uint8_t dst_patched_size, const char *src_file, uint32_t src_offset);
+void bios_linker_loader_cleanup(BIOSLinker *linker);
 
 #endif /* HW_FW_BUILD_H_ */
