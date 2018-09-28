ceab4f6c428c52597322bb5e6795ab5b8af4a264
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-09-28 15:42:23 +0900

    Update memory.c

--- memory.c
+++ memory.c
@@ -5,6 +5,22 @@
  *      Student (MIG Virtual Developer): Tung Dang
  */
 
-
+#include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "qemu-common.h"
+#include "qom/cpu.h"
+#include "exec/memory.h"
+#include "exec/address-spaces.h"
+#include "qapi/visitor.h"
+#include "qemu/bitops.h"
+#include "qemu/error-report.h"
+#include "qom/object.h"
+#include "exec/memory-internal.h"
+#include "exec/ram_addr.h"
+#include "sysemu/kvm.h"
+#include "sysemu/sysemu.h"
+#include "hw/qdev-properties.h"
+#include "hw/misc/mmio_interface.h"
+#include "migration/vmstate.h"
 
 
