49584d567ee76cd577ccda20dc1bddab26feb317
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-10-02 14:22:08 +0900

    Update memory.c

--- memory.c
+++ memory.c
@@ -1217,3 +1217,156 @@ static const MemoryRegionOps ram_device_mem_ops = {
 		.unaligned = true,
 	},
 };
+
+bool memory_region_access_valid(MemoryRegion *mr, hwaddr addr, unsigned size,
+		                        bool is_write, MemTxAttrs attrs) {
+	int access_size_min, access_size_max;
+	int access_size, i;
+	if (!mr->ops->valid.unaligned && (addr & (size - 1)))
+	{
+		return false;
+	}
+	if (!mr->ops->valid.accepts)
+	{
+		return true;
+	}
+	access_size_min = mr->ops->valid.min_access_size;
+	if (!mr->ops->valid.min_access_size)
+	{
+		access_size_min = 1;
+	}
+	access_size_max = mr->ops->valid.max_access_size;
+	if (!mr->ops->valid.max_access_size)
+	{
+		access_size_max = 4;
+	}
+	access_size = MAX(MIN(size,access_size_max),access_size_min);
+	for (i = 0; i < size; i += access_size)
+	{
+		if (!mr->ops->valid.accepts(mr->opaque,addr + i, access_size, is_write, attrs))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+static MemTxResult memory_region_dispatch_read1(MemoryRegion *mr, hwaddr addr, uint64_t *pval,
+		                                        unsigned size, MemTxAttrs attrs) {
+	*pval = 0;
+	if (mr->ops->read)
+	{
+		return access_with_adjusted_size(addr, pval, size,
+				                         mr->ops->impl.min_access_size,
+										 mr->ops->impl.max_access_size,
+										 memory_region_read_accessor,
+										 mr,attrs);
+	}
+	else if (mr->ops->read_with_attrs)
+	{
+		return access_with_adjusted_size(addr, pval, size,
+				                         mr->ops->impl.min_access_size,
+										 mr->ops->impl.max_access_size,
+										 memory_region_read_with_attrs_accessor,
+										 mr, attrs);
+	}
+	else
+	{
+		return access_with_adjusted_size(addr, pval, size, 1, 4,
+				                         memory_region_oldmmio_read_accessor,
+										 mr, attrs);
+	}
+}
+
+MemTxResult memory_region_dispatch_read(MemoryRegion *mr, hwaddr addr, uint64_t *pval,
+		                                unsigned size, MemTxAttrs attrs) {
+	MemTxResult r;
+	if (!memory_region_access_valid(mr,addr,size,false,attrs))
+	{
+		*pval = unsingned_mem_read(mr,addr,size);
+		return MEMTX_DECODE_ERROR;
+	}
+	r = memory_region_dispatch_read1(mr, addr, pval, size, attrs);
+	adjust_endianness(mr,pval,size);
+	return r;
+}
+
+static bool memory_region_dispatch_write_eventfds(MemoryRegion *mr, hwaddr addr, uint64_t data,
+		                                          unsigned size, MemTxAttrs attrs) {
+	MemoryRegionIoeventfd ioeventfd = {
+		.addr = addrrange_make(int128_make64(addr), int128_make64(size)),
+		.data = data,
+	};
+	unsigned i;
+	for (i=0; i < mr->ioventfd_nb; i++)
+	{
+		ioeventfd.match_data = mr->ioeventfds[i].match_data;
+		ioeventfd.e = mr->ioeventfds[i].e;
+		if (memory_region_ioeventfd_equal(&ioeventfd, &mr->ioeventfds[i]))
+		{
+			event_notifier_set(ioeventfd.e);
+			return true;
+		}
+	}
+	return false;
+}
+
+MemTxResult memory_region_dispatch_write(MemoryRegion *mr, hwaddr addr, uint64_t data,
+		                                 unsigned size, MemTxAttrs attrs) {
+	if (!memory_region_access_valid(mr,addr,size, true, attrs))
+	{
+		unassigned_mem_write(mr,addr,data,size);
+		return MEMTX_DECODE_ERROR;
+	}
+	adjust_endianness(mr, &data, size);
+	if ((!kvm_eventfds_enabled()) &&
+		memory_region_dispatch_write_eventfds(mr,addr,data,size,attrs))
+	{
+		return MEMTX_OK;
+	}
+	if (mr->ops->write)
+	{
+		return access_with_adjusted_size(addr,&data,size,
+				                         mr->ops->impl.min_access_size,
+										 mr->ops->impl.max_access_size,
+										 memory_region_write_accessor,
+										 mr, attrs);
+	}
+	else if (mr->ops->write_with_attrs)
+	{
+		return access_with_adjusted_size(addr,&data,size,
+						                 mr->ops->impl.min_access_size,
+										 mr->ops->impl.max_access_size,
+										 memory_region_write_with_attrs_accessor,
+										 mr, attrs);
+	}
+	else
+	{
+		return access_with_adjusted_size(addr, &data, size, 1, 4,
+					                     memory_region_oldmmio_write_accessor,
+										 mr, attrs);
+	}
+}
+
+void memory_region_init_io(MemoryRegion *mr, Object *owner, const MemoryRegionOps *ops,
+		                   void *opaque, const char *name, uint64_t size) {
+	memory_region_init(mr,owner,name,size);
+	mr->ops = ops ? ops : &unassigned_mem_ops;
+	mr->opaque = opaque;
+	mr->terminates = true;
+}
+
+void memory_region_init_ram_nomigrate(MemoryRegion *mr, Object *owner, const char *name,
+		                              uint64_t size, Error **errp) {
+	memory_region_init_ram_shared_nomigrate(mr, owner, name, size, false, errp);
+}
+
+void memory_region_init_ram_shared_nomigrate(MemoryRegion *mr, Object *owner,
+		                       const char *name, uint64_t size, bool share, Error **errp) {
+	memory_region_init(mr,owner,name,size);
+	mr->ram = true;
+	mr->terminates = true;
+	//mr->destructor = memory_region_destructor_ram;
+	mr->ram_block = qemu_ram_alloc(size, share, mr, errp);
+	mr->dirty_log_mask = tcg_enabled() ? (1 << DIRTY_MEMORY_CODE) : 0;
+}
