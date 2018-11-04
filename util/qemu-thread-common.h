2bb6086db0f3988e3012384693ff38536d488a18
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-04 13:13:52 +0900

    Update qemu-thread-common.h

--- util/qemu-thread-common.h
+++ util/qemu-thread-common.h
@@ -8,6 +8,38 @@
 #ifndef UTIL_QEMU_THREAD_COMMON_H_
 #define UTIL_QEMU_THREAD_COMMON_H_
 
+#include "qemu/typedefs.h"
+#include "qemu/thread.h"
+#include "qemu/thread-posix.h"
 
+static inline void qemu_mutex_post_init(QemuMutex *mutex) {
+#ifdef CONFIG_DEBUG_MUTEX
+	mutex->file = NULL;
+	mutex->line = 0;
+#endif
+	mutex->initialized = true;
+}
+
+static inline void qemu_mutex_pre_lock(QemuMutex *mutex, const char *file, int line ) {
+	trace_qemu_mutex_lock(mutex, file, line);
+}
+
+static inline void qemu_mutex_post_lock(QemuMutex *mutex, const char *file, int line) {
+#ifdef CONFIG_DEBUG_MUTEX
+    mutex->file = file;
+    mutex->line = line;
+#endif
+
+    trace_qemu_mutex_locked(mutex, file, line);
+}
+
+static inline void qemu_mutex_pre_unlock(QemuMutex *mutex, const char *file, int line) {
+#ifdef CONFIG_DEBUG_MUTEX
+    mutex->file = NULL;
+    mutex->line = 0;
+#endif
+
+    trace_qemu_mutex_unlock(mutex, file, line);
+}
 
 #endif /* UTIL_QEMU_THREAD_COMMON_H_ */
