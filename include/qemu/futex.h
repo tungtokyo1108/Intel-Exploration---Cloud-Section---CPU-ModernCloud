2a87d9b683bfe95db624101bd3af7f7968cbc204
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-05 14:29:14 +0900

    Update and compare futex.h with Facebook

--- include/qemu/futex.h
+++ include/qemu/futex.h
@@ -11,6 +11,28 @@
 #ifndef QEMU_FUTEX_H_
 #define QEMU_FUTEX_H_
 
+#include <sys/syscall.h>
+#include <linux/futex.h>
 
+#define qemu_futex(...) syscall(__NR_futex, __VA_ARGS__)
+
+static inline void qemu_futex_wake(void *f, int n)
+{
+	qemu_futex(f, FUTEX_WAKE, n, NULL, NULL, 0);
+}
+
+static inline void qemu_futex_wait(void *f, unsigned val)
+{
+    while (qemu_futex(f, FUTEX_WAIT, (int) val, NULL, NULL, 0)) {
+        switch (errno) {
+        case EWOULDBLOCK:
+            return;
+        case EINTR:
+            break; /* get out of switch and retry */
+        default:
+            abort();
+        }
+    }
+}
 
 #endif /* QEMU_FUTEX_H_ */
