2a6734964cfbfe8a60676e095b751634bbb8f4ec
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-05 23:50:24 +0900

    Update qemu-thread-posix.c

--- util/qemu-thread-posix.c
+++ util/qemu-thread-posix.c
@@ -210,5 +210,177 @@ void qemu_sem_destroy(QemuSemaphore *sem)
 #endif
 }
 
+void qemu_sem_post(QemuSemaphore *sem)
+{
+	int rc;
+	assert(sem->initialized);
+#ifndef CONFIG_SEM_TIMEDWAIT
+	pthread_mutex_lock(&sem->lock);
+	if (sem->count == UINT_MAX)
+	{
+		rc = EINVAL;
+	}
+	else
+	{
+		sem->count++;
+		rc = pthread_cond_signal(&sem->cond);
+	}
+	pthread_mutex_unlock(&sem->lock);
+	if (rc != 0)
+	{
+		error_exit(rc, __func__);
+	}
+#else
+	rc = sem_post(&sem->sem);
+	    if (rc < 0) {
+	        error_exit(errno, __func__);
+	}
+#endif
+}
 
+static void compute_abs_deadline(struct timespec *ts, int ms)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	ts->tv_nsec = tv.tv_usec * 1000 + (ms % 1000) * 1000000;
+	ts->tv_sec = tv.tv_sec + ms/1000;
+	if (ts->tv_nsec >= 1000000000)
+	{
+		ts->tv_sec++;
+		ts->tv_nsec -= 1000000000;
+	}
+}
+
+int qemu_sem_timedwait(QemuSemaphore *sem, int ms)
+{
+	int rc;
+	struct timespec ts;
+	assert(sem->initialized);
+#ifndef CONFIG_SEM_TIMEDWAIT
+	rc = 0;
+	compute_abs_deadline(&ts, ms);
+	pthread_mutex_lock(&sem->lock);
+	while (sem->count == 0)
+	{
+		rc = pthread_cond_timedwait(&sem->cond, &sem->lock, &ts);
+		if (rc == ETIMEDOUT)
+		{
+			break;
+		}
+		if (rc != 0)
+		{
+			error_exit(rc, __func__);
+		}
+	}
+	if (rc != ETIMEDOUT)
+	{
+		--sem->count;
+	}
+	pthread_mutex_unlock(&sem->lock);
+	return (rc == ETIMEDOUT ? -1 : 0);
+#else
+	if (ms <= 0) {
+	        /* This is cheaper than sem_timedwait.  */
+	        do {
+	            rc = sem_trywait(&sem->sem);
+	        } while (rc == -1 && errno == EINTR);
+	        if (rc == -1 && errno == EAGAIN) {
+	            return -1;
+	        }
+	    } else {
+	        compute_abs_deadline(&ts, ms);
+	        do {
+	            rc = sem_timedwait(&sem->sem, &ts);
+	        } while (rc == -1 && errno == EINTR);
+	        if (rc == -1 && errno == ETIMEDOUT) {
+	            return -1;
+	        }
+	    }
+	    if (rc < 0) {
+	        error_exit(errno, __func__);
+	    }
+	return 0;
+#endif
+}
+
+void qemu_sem_wait(QemuSemaphore *sem)
+{
+	int rc;
+	assert(sem->initialized);
+#ifndef CONFIG_SEM_TIMEDWAIT
+	pthread_mutex_lock(&sem->lock);
+	while (sem->count == 0)
+	{
+		rc = pthread_cond_wait(&sem->cond, &sem->lock);
+		if (rc != 0)
+		{
+			error_exit(rc, __func__);
+		}
+	}
+	--sem->count;
+	pthread_mutex_unlock(&sem->lock);
+#else
+	do {
+	        rc = sem_wait(&sem->sem);
+	    } while (rc == -1 && errno == EINTR);
+	    if (rc < 0) {
+	        error_exit(errno, __func__);
+	}
+#endif
+}
+
+#ifdef __linux__
+#include "qemu/futex.h"
+#else
+static inline void qemu_futex_wake(QemuEvent *ev, int n)
+{
+	assert(ev->initialized);
+	pthread_mutex_lock(&ev->lock);
+	if (n == 1)
+	{
+		pthread_cond_signal(&ev->cond);
+	}
+	else
+	{
+		pthread_cond_broadcast(&ev->cond);
+	}
+	pthread_mutex_unlock(&ev->lock);
+}
+
+static inline void qemu_futex_wait(QemuEvent *ev, unsigned val)
+{
+	assert(ev->initialized);
+	pthread_mutex_lock(&ev->lock);
+	if (ev->value == val)
+	{
+		pthread_cond_wait(&ev->cond, &ev->lock);
+	}
+	pthread_mutex_unlock(&ev->lock);
+}
+#endif
+
+#define EV_SET         0
+#define EV_FREE        1
+#define EV_BUSY       -1
+
+void qemu_event_init(QemuEvent *ev, bool init)
+{
+#ifndef __linux__
+	pthread_mutex_init(&ev->lock, NULL);
+	pthread_cond_init(&ev->cond, NULL);
+#endif
+
+	ev->value = (init ? EV_SET : EV_FREE);
+	ev->initialized = true;
+}
+
+void qemu_event_destroy(QemuEvent *ev)
+{
+	assert(ev->initialized);
+	ev->initialized = false;
+#ifndef __linux__
+    pthread_mutex_destroy(&ev->lock);
+    pthread_cond_destroy(&ev->cond);
+#endif
+}
 
