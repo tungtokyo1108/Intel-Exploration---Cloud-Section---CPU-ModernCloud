1339c90c19ed9d311da0e97940c22039ad412206
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-05 14:06:20 +0900

    Add files via upload

---
+++ include/qemu/futex.h
@@ -0,0 +1,16 @@
+/*
+ * futex.h
+ *
+ *  Created on: Nov 5, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ *
+ *  Compared with Facebook:
+ *  https://github.com/facebook/folly/blob/master/folly/detail/Futex.h
+ */
+
+#ifndef QEMU_FUTEX_H_
+#define QEMU_FUTEX_H_
+
+
+
+#endif /* QEMU_FUTEX_H_ */
