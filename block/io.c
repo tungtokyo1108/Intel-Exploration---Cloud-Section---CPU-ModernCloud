5c92af63b74aa7ff309e93516b04a9141f7e8c0e
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-11-30 22:30:49 +0900

    Update io.c

--- block/io.c
+++ block/io.c
@@ -1,1 +1,17 @@
+/*
+ * io.c
+ * https://github.com/intel/nemu/blob/topic/virt-x86/block/io.c
+ *
+ *  Created on: Nov 27, 2018
+ *      Student (MIG Virtual Developer): Tung Dang
+ */
 
+#include "qemu/osdep.h"
+#include "sysemu/block-backend.h"
+#include "block/aio-wait.h"
+#include "block/blockjob.h"
+#include "block/blockjob_int.h"
+#include "block/block_int.h"
+#include "qemu/cutils.h"
+#include "qapi/error.h"
+#include "qemu/error-report.h"
