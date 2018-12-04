f2f7eba0b0ce5b0a8b2a9e73f81d7d0d3507fec9
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-12-04 23:44:36 +0900

    Update io.c

--- block/io.c
+++ block/io.c
@@ -15,3 +15,134 @@
 #include "qemu/cutils.h"
 #include "qapi/error.h"
 #include "qemu/error-report.h"
+
+#define NOT_DONE 0x7fffffff
+
+/*Maximum bounce buffer for copy-on-read and write zeroes, in bytes*/
+#define MAX_BOUNCE_BUFFER (32768 << BDRV_SECTOR_BITS)
+static AioWait drain_all_aio_wait;
+
+static void bdrv_parent_cb_resize(BlockDriverState *bs);
+static int coroutine_fn bdrv_co_do_pwrite_zeroes(BlockDriverState *bs, int offset, int bytes,
+                                                 BdrvRequestFlags flags);
+
+void bdrv_parent_drained_begin(BlockDriverState* bs, BdrvChild *ignore, bool ignore_bds_parents) 
+{
+    BdrvChild *c;
+    BdrvChild *next;
+
+    QLIST_FOREACH_SAFE(c,&bs->parent, next_parent, next) {
+        if (c == ignore || (ignore_bds_parents && c->role->parent_is_bds)) 
+        {
+            continue;
+        }
+        bdrv_parent_drained_begin_single(c, false);
+    } 
+} 
+
+void bdrv_parent_drained_end(BlockDriverState *bs, BdrvChild *ignore, bool ignore_bds_parents)
+{
+    BdrvChild *c;
+    BdrvChild *next;
+
+    QLIST_FOREACH_SAFE(c, &bs->parent, next_parent, next) {
+        if (c == ignore || (ignore_bds_parents && c->role->parent_is_bds)) {
+            continue;
+        }
+        if (c->role->drained_end)
+        {
+            c->role->drained_end(c);
+        }
+    }
+}
+
+static bool bdrv_parent_drained_poll_single(BdrvChild *c) 
+{
+    if (c->role->drained_poll)
+    {
+        return c->role->drained_poll(c);
+    }
+    return false;
+}
+
+static bool bdrv_parent_drained_poll(BlockDriverState *bs, BdrvChild *ignore, 
+                                     bool ignore_bds_parents)
+{
+    BdrvChild *c;
+    BdrvChild *next;
+    bool busy = false;
+
+    QLIST_FOREACH_SAFE(c, &bs->parents, next_parent, next) {
+        if (c == ignore || (ignore_bds_parents && c->role->parent_is_bds)) {
+            continue;
+        }
+        busy |= bdrv_parent_drained_poll_single(c);
+    }
+    return busy;
+}                                    
+
+void bdrv_parent_drained_begin_single(BdrvChild *c, bool poll)
+{
+    if (c->role->drained_begin)
+    {
+        c->role->drained_begin(c);
+    }
+    if (poll)
+    {
+        BDRV_POLL_WHILE(c->bs, bdrv_parent_drained_poll_single(c));
+    }
+}
+
+static void bdrv_merge_limits(BlockLimits *dst, const BlockLimits *src) 
+{
+    dst->opt_transfer = MAX(dst->opt_transfer, src->opt_transfer);
+    dst->max_transfer = MIN_NON_ZERO(dst->max_transfer, src->max_transfer);
+    dst->opt_mem_aligment = MAX(dst->opt_mem_aligment, src->alignment);
+    dst->min_mem_aligment = MAX(dst->min_mem_aligment, src->min_mem_aligment);
+    dst->max_iov = MIN_NON_ZERO(dst->max_iov, src->max_iov);
+}
+
+void bdrv_refresh_limits(BlockDriverState *bs, Error **errp) 
+{
+    BlockDriver *drv = bs->drv;
+    Error *local_err = NULL;
+    memset(&bs->bl,0,sizeof(bs->bl));
+    if (!drv)
+    {
+        return;
+    }
+    bs->bl.request_aligment = (drv->bdrv_co_preadv || 
+                               drv->bdrv_aio_preadv) ? 1 : 512;
+
+    if (bs->file)
+    {
+        bdrv_refresh_limits (bs->file->bs, &local_err);
+        if (local_err)
+        {
+            error_propagate(errp, local_err);
+            return;
+        }
+        bdrv_merge_limits(&bs->bl, &bs->file->bs->bl);
+    }                           
+    else
+    {
+        bs->bl.min_mem_aligment = 512;
+        bs->bl.opt_mem_aligment = getpagesize();
+        bs->bl.max_iov = IOV_MAX;
+    }
+
+    if (bs->backing)
+    {
+        bdrv_refresh_limits(bs->backing_bs, &local_err);
+        if (local_err)
+        {
+            error_propagate(errp, local_err);
+            return;
+        }
+        bdrv_merge_limits(&bs->bl, &bs->backing->bs->bl);
+    }
+    if (drv->bdrv_refresh_limits)
+    {
+        drv->bdrv_refresh_limits(bs, errp);
+    }
+}
